c97291275dc8a1f5e89d04e35b373327701d56e2
Johongir <162979240+Kblv37@users.noreply.github.com>
2026-03-17 16:54:10 +0500

    Кобилов добавил свой код

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from proekt640AxmedovDilmurod import funk640DILMUROD
 from funcKosyanenko import funk640BOGDAN
+from proektQobilov import funkQobilov
 
 y = int(input("Введите число y: "))
 funk640DILMUROD(y)
@@ -9,6 +10,10 @@ funk640DILMUROD(y)
 x = int(input("Enter your x"))
 funk640BOGDAN(x)
 
+# Кобилов
+num = int(input("Введите число num для возведения в квадрат: "))
+result = funkQobilov(num)
+print("Квадрат числа", num, "равен", result)
 
 ###
 
